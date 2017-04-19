000fa56c8a55631e5115fa1bd62b91933c431d54
James Monger <jameskmonger@hotmail.co.uk>
2017-04-19 13:03:43 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+const get32BitInt = (val: number) => val | 0; // coerce to a 32 bit int
+
+export default get32BitInt;
---
+++ test.ts
@@ -0,0 +1,48 @@
+import { TestFixture, Test, TestCase, Expect } from "alsatian";
+
+import get32BitInt from "./index";
+
+const MIN_32_BIT = -2147483648;
+const MAX_32_BIT = 2147483647;
+
+@TestFixture()
+export class Get32BitIntTests {
+
+  @TestCase(5.003, 5)
+  @TestCase(100.192, 100)
+  @TestCase(128412.12938, 128412)
+  public shouldRoundFloatDown(input: number, expectedOutput: number) {
+    const output = get32BitInt(input);
+
+    Expect(output).toBe(expectedOutput);
+  }
+
+  @TestCase(100)
+  @TestCase(300)
+  @TestCase(435345)
+  @TestCase(14873874)
+  @TestCase(MAX_32_BIT)
+  public shouldWrapCorrectlyWhenAboveMax(numberToAdd: number) {
+    const expectedOutput = MIN_32_BIT - 1 + numberToAdd;
+    const input = MAX_32_BIT + numberToAdd;
+
+    const output = get32BitInt(input);
+
+    Expect(output).toBe(expectedOutput);
+  }
+
+  @TestCase(100)
+  @TestCase(300)
+  @TestCase(435345)
+  @TestCase(14873874)
+  @TestCase(MAX_32_BIT)
+  public shouldWrapCorrectlyWhenBelowMin(numberToSubtract: number) {
+    const expectedOutput = MAX_32_BIT + 1 - numberToSubtract;
+    const input = MIN_32_BIT - numberToSubtract;
+
+    const output = get32BitInt(input);
+
+    Expect(output).toBe(expectedOutput);
+  }
+
+}
